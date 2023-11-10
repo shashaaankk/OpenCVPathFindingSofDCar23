9b5906ccfbacd117233da7c11cd6a4e376496182
Katrin Bauer <44502936+KLBa@users.noreply.github.com>
2023-11-10 19:32:39 +0100

    Add marker detection MWE

---
+++ marker_detection/aruco_detection_mwe.py
@@ -0,0 +1,23 @@
+
+import cv2
+
+dict_id = cv2.aruco.DICT_4X4_1000
+marker_type = 'DICT_4X4_1000'
+
+print("[INFO] detecting '{}' tags...".format(marker_type))
+# setup aruco detector
+arucoDict = cv2.aruco.getPredefinedDictionary(dict_id)
+arucoParams = cv2.aruco.DetectorParameters()
+detector = cv2.aruco.ArucoDetector(arucoDict, arucoParams)
+
+# read image
+frame = cv2.imread('4x4_1000-10_phone_photo.jpg')
+
+# detect markers given in the arucoDict
+markerCorners, markerIds, rejectedCandidates = detector.detectMarkers(frame)
+
+print(
+    markerCorners,
+    markerIds,
+    rejectedCandidates
+)
