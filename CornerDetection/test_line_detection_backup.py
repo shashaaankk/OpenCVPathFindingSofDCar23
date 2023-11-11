854de52b0cf731535587bc37b5a1ddb82953e297
Katrin Bauer <44502936+KLBa@users.noreply.github.com>
2023-11-11 11:28:50 +0100

    Backup

--- CornerDetection/test_line_detection_backup.py
+++ CornerDetection/test_line_detection_backup.py
@@ -5,7 +5,44 @@ import numpy as np
 # import matplotlib
 import matplotlib.pyplot as plt
 # from matplotlib.colors import rgb_to_hsv, hsv_to_rgb
-import sklearn
+
+from sklearn.cluster import KMeans
+
+def cluster_lines(lines):
+    # Normalize line directions using sine and cosine of the angles
+    line_directions = [
+        line[0, :2] - line[0, 2:]
+        for line in lines 
+    ]
+    line_directions = [
+        (vec[:, None] @ vec[None, :] / np.linalg.norm(vec)**2).flatten()
+        for vec in line_directions 
+    ]
+    print(line_directions)
+
+    # Use KMeans to cluster lines into two directions
+    kmeans = KMeans(n_clusters=2, random_state=0).fit(line_directions)
+
+    # Get the labels assigned to each line
+    labels = kmeans.labels_
+
+    # Draw lines based on their direction
+    # result_image = image.copy()
+    # for i, line in enumerate(lines):
+    #     rho, theta = line[0]
+    #     a = np.cos(theta)
+    #     b = np.sin(theta)
+    #     x0 = a * rho
+    #     y0 = b * rho
+    #     x1 = int(x0 + 1000 * (-b))
+    #     y1 = int(y0 + 1000 * (a))
+    #     x2 = int(x0 - 1000 * (-b))
+    #     y2 = int(y0 - 1000 * (a))
+# 
+    #     # Draw lines in different colors based on their cluster label
+    #     color = (0, 0, 255) if labels[i] == 0 else (255, 0, 0)
+    #     cv2.line(result_image, (x1, y1), (x2, y2), color, 2)
+    return labels
 
 def thinning_zhangsuen(image):
     size = np.size(image)
@@ -104,33 +141,95 @@ def line_detection(gray_scale):
         minLineLength=gray_scale.shape[0] / 10,
         maxLineGap=gray_scale.shape[0] / 10
     )
+    # Sort lines based on confidence (line length)
+    lines = np.array(sorted(lines, key=lambda x: x[0, 2], reverse=True))
+
     # lines = detector.detect(gray_scale)
     print(lines)
-    # Sort lines based on confidence (line length)
-    lines = sorted(lines, key=lambda x: x[0, 2], reverse=True)
+
+    labels = cluster_lines(lines)
+
+    print('clusters', labels)
+
+    c0 = lines[labels == 0]
+    c1 = lines[labels == 1]
 
     # Extract the two most confident lines
-    top_lines = lines[:2]
+    top_lines = [c0[0], c1[0]]
     plt.figure()
     plt.imshow(gray_scale, cmap='gray')
     if lines is None:
         lines = []
         print('No lines!')
+    
+    for l in lines:
+        plt.plot(l[0,::2], l[0,1::2], color='blue', label='all')
+    
+
     for l in top_lines:
         print(l)
         print(angle(l[0,:2], l[0,2:]))
-        plt.plot(l[0,::2], l[0,1::2], color='red')
+        plt.plot(l[0,::2], l[0,1::2], color='red', label='representatnts')
     rhos = []
 
+    return np.array(top_lines)
+
+def intersect_lines(l1, l2):
+    '''
+    line represented as (x1, y1, x2, y2)
+    where (x1,y1) and (x2, y2) are points on the line.
+    '''
+    v1 = l1[:2] - l1[2:]
+    v2 = l2[:2] - l2[2:]
+
+    A = np.array([- v1, v2])
+    b = l2[2:] - l1[2:]
+
+    t = np.linalg.solve(A, b)
+    print(l1[2:] + t[0] * v1)
+    print(l2[2:] + t[1] * v2)
+
+    x = l2[2:] + t[1] * v2
+    plt.scatter(x[0], x[1])
+
+    # return l2[2:] + t[1] * v2
+
+def noah_preproc(image):
+    retval, img_thresh = cv2.threshold(image[:, :, 1], 150, 255, cv2.THRESH_BINARY)
+
+
+    # erosion
+    kernel = np.ones((5, 5), np.uint8) 
+    
+    # Using cv2.erode() method  
+    image_eroded = cv2.erode(img_thresh, kernel, iterations=5)
+
+
+    #dilation
+    img_dilation = cv2.dilate(image_eroded, kernel, iterations=5)
+
+
+    # edge thining
+    thinned_image = thinning_zhangsuen(img_dilation)
+
+    return thinned_image
+
 img = cv2.imread('frame1.png')
-img = cv2.imread('frameLshape.jpg')
-img = threshold_rgb(img)
-# edge_detection(img)
-# rgb_gray_scale = np.where(img, np.array([255, 255, 255]), np.array([0, 0, 0]))
-img = thinning_zhangsuen(img)
+# # img = cv2.imread('frameLshape.jpg')
+# img = cv2.resize(img, (256, 512))
+# img = threshold_rgb(img)
+# # edge_detection(img)
+# # rgb_gray_scale = np.where(img, np.array([255, 255, 255]), np.array([0, 0, 0]))
+# img = thinning_zhangsuen(img)
+
 
 plt.figure()
 plt.imshow(img)
+
+img = noah_preproc(img)
+img = cv2.dilate(img, np.ones((3, 3)))
+plt.figure()
+plt.imshow(img, cmap='gray')
 
 print('after thinning', img.shape, img.dtype)
 img_bin = np.zeros_like(img)
@@ -138,6 +237,8 @@ img_bin[img > 1] = 255
 
 print('after thresholding', img.shape, img.dtype)
 
-line_detection(img)
+# lines = line_detection(img)
+
+# intersection_point = intersect_lines(lines[0, 0], lines[1, 0])
 
 plt.show()
