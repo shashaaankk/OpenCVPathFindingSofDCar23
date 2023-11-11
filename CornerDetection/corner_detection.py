0d889bf5c8c4887cdeae6f1d26dbd182300746a7
Katrin Bauer <44502936+KLBa@users.noreply.github.com>
2023-11-11 14:38:27 +0100

    Update corner detection
    * works for most data images except frame1

--- CornerDetection/corner_detection.py
+++ CornerDetection/corner_detection.py
@@ -14,6 +14,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 
+def merge_clusters(lines, labels, angle_threshold=0.5):
+    c0 = lines[labels == 0]
+    c1 = lines[labels == 1]
+    top_lines = []
+    top_lines.append(c0[0])
+    if len(c1) > 0:
+        v0 = np.array(c0[0][0][:2] - c0[0][0][2:])
+        v1 = np.array(c1[0][0][:2] - c1[0][0][2:])
+
+        print(v0)
+        inverse_angle = np.sum(v0 * v1) / (np.linalg.norm(v0) * np.linalg.norm(v1))
+        print('dotproduct', inverse_angle)
+        if np.abs(inverse_angle) < angle_threshold:
+            # Extract the two most confident lines
+            top_lines.append(c1[0])
+    return top_lines
+
 def cluster_lines(lines):
     # Normalize line directions using sine and cosine of the angles
     line_directions = [
@@ -26,12 +43,16 @@ def cluster_lines(lines):
     ]
     print(line_directions)
 
-    # Use KMeans to cluster lines into two directions
-    kmeans = KMeans(n_clusters=2, random_state=0).fit(line_directions)
+    for i in range(3):
+        # Use KMeans to cluster lines into two directions
+        kmeans = KMeans(n_clusters=2, random_state=i).fit(line_directions)
 
-    # Get the labels assigned to each line
-    labels = kmeans.labels_
+        # Get the labels assigned to each line
+        labels = kmeans.labels_
 
+        if len(merge_clusters(lines, labels)) == 2:
+            return labels
+    
     return labels
 
 def thinning_zhangsuen(image):
@@ -61,33 +82,29 @@ def line_detection(gray_scale, angle_threshold = .5):
         rho=1,
         theta=np.pi / 90,
         threshold=100,
-        minLineLength=gray_scale.shape[0] / 5,
+        minLineLength=gray_scale.shape[0] / 10,
         maxLineGap=gray_scale.shape[0] / 10
     )
+    if lines is None:
+        print('[Error] no line detected')
+        return None
     # Sort lines based on confidence (line length)
     lines = np.array(sorted(lines, key=lambda x: x[0, 2], reverse=True))
 
+    # plt.imshow(img)
+    # 
+    # for l in lines:
+    #     plt.plot(l[0,::2], l[0,1::2], color='blue')
+    # 
+    # plt.colorbar()
+    # plt.show()
     # lines = detector.detect(gray_scale)
     print(lines)
 
     labels = cluster_lines(lines)
-
     print('clusters', labels)
 
-    c0 = lines[labels == 0]
-    c1 = lines[labels == 1]
-    top_lines = []
-    top_lines.append(c0[0])
-    if len(c1) > 0:
-        v0 = np.array(c0[0][0][:2] - c0[0][0][2:])
-        v1 = np.array(c1[0][0][:2] - c1[0][0][2:])
-
-        print(v0)
-        inverse_angle = np.sum(v0 * v1) / (np.linalg.norm(v0) * np.linalg.norm(v1))
-        print('dotproduct', inverse_angle)
-        if np.abs(inverse_angle) < angle_threshold:
-            # Extract the two most confident lines
-            top_lines.append(c1[0])
+    top_lines = merge_clusters(lines, labels)
 
     return np.array(top_lines)
 
@@ -120,25 +137,24 @@ def intersect_lines(l1, l2):
     )
 
 def preprocess(image):
-    retval, img_thresh = cv2.threshold(image[:, :, 1], 170, 255, cv2.THRESH_BINARY)
-
+    retval, img = cv2.threshold(image[:, :, 1], 170, 255, cv2.THRESH_BINARY)
 
     # erosion
-    n = image.shape[0] // 10
-    kernel = np.ones((n, n), np.uint8) 
-    
-    # Using cv2.erode() method  
-    image_eroded = cv2.erode(img_thresh, kernel)
+    n = image.shape[0] // 200
+    kernel = np.ones((n, n), np.uint8)
+    # Using cv2.erode() method
+    #img = cv2.erode(img, kernel, iterations=1)
 
-
-    #dilation
-    img_dilation = cv2.dilate(image_eroded, kernel)
-
+    # dilation
+    # img = cv2.dilate(img, kernel, iterations=2)
 
     # edge thining
-    thinned_image = thinning_zhangsuen(img_thresh)
+    img = thinning_zhangsuen(img)
+    # plt.imshow(img)
+    # plt.colorbar()
+    # plt.show()
 
-    return thinned_image
+    return img
 
 def extract_lines_and_corner(img: np.array):
     '''
@@ -156,12 +172,13 @@ def extract_lines_and_corner(img: np.array):
         a pair of
         lines: 1 x 4 array with end points of one line
         corner: None
-        
     '''
     img = preprocess(img)
     img = cv2.dilate(img, np.ones((3, 3)))
 
     lines = line_detection(img)
+    if lines is None:
+        return None, None
     
     intersection_point = None
     if len(lines) > 1:
@@ -172,21 +189,33 @@ def extract_lines_and_corner(img: np.array):
 
 if __name__ == '__main__':
     # This is executed if we call `python corner_detection.py` via the command line
-    img_name = 'frame1.png'
+    import os
 
-    img = cv2.imread(img_name)
-    img = img[10:, 10:]
+    for img_name in os.listdir('data'):
+    # for img_name in ['frameLshape.jpg']:
+        # img_name = 'frameLshape3.jpg'
 
-    lines, corner = extract_lines_and_corner(img)
+        img = cv2.imread(f'data/{img_name}')
+        assert img is not None
+        img = img[10:, 10:]
 
-    # plot preprocessed image
-    plt.imshow(img)
+        lines, corner = extract_lines_and_corner(img)
 
-    # plot lines
-    for l in lines:
-        plt.plot(l[::2], l[1::2], color='red', label='all')
+        # plot preprocessed image
+        plt.imshow(img)
 
-    # plot corner
-    plt.scatter(corner[0], corner[1], color='green')
-    plt.savefig(f'output/lines_and_corner_{img_name}')
-    plt.show()
+        # plot lines
+        if lines is not None:
+            for l in lines:
+                plt.plot(l[::2], l[1::2], color='red', label='all')
+
+        # plot corner
+        if corner is not None:
+            print('corner detected')
+            plt.scatter(corner[0], corner[1], s=100, marker='x', color='pink', linewidths=3)
+        else:
+            print('no corner')
+        
+        plt.savefig(f'output/lines_and_corner_{img_name}')
+        plt.figure()
+        # plt.show()
